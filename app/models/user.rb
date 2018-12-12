75501dd5138bd8bdd9856af90a8b8ba1f75d3818
Greg Morgan <gregm9999@gmail.com>
2018-12-12 23:24:54 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  class Users < ActiveRecord::Base
+  def change
+    create_table :users do |user|
+      user.string :name
+      user.string :email
+      user.string :password
+    end
+  end
+
+end
 end
